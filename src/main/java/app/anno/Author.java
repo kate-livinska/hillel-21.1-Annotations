f488506324ede06c3d50f14b5b79f844df58e74e
Kateryna Livinska <kateryna.livinska@gmail.com>
2024-12-10 21:20:00 +0100

    Add Author annotation

---
+++ src/main/java/app/anno/Author.java
@@ -0,0 +1,13 @@
+package app.anno;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Author {
+    String firstName();
+    String lastName();
+}
